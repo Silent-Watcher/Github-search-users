08b43eab1b789bba89a6dbaca91023f3d5ba214d
Ali.t.nazari <alitabatabaee20@gmail.com>
2023-03-20 01:56:44 +0330

    add heading

--- src/js/app.js
+++ src/js/app.js
@@ -10,4 +10,16 @@ window.addEventListener('load', () => {
     search.classList.toggle('active');
     input.focus();
   });
+
+  //   header
+  const enhance = () => {
+    document.querySelectorAll('.word').forEach((word) => {
+      const letters = word.innerText.split('');
+      word.innerHTML = null;
+      letters.forEach((letter) => {
+        word.innerHTML += `<span class="letter">${letter}</span>`;
+      });
+    });
+  };
+  enhance();
 });
